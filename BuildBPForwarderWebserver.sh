8942ab300f7df70a2cc67fc053fa785d9620da3a
WD-Dissertation <114887074+WD-Dissertation@users.noreply.github.com>
2022-10-07 22:17:16 +1100

    Add files via upload

--- BuildBPForwarderWebserver.sh
+++ BuildBPForwarderWebserver.sh
@@ -5,13 +5,19 @@ apt-get update
 
 apt-get install python3-pip python3-dev build-essential libssl-dev libffi-dev python3-setuptools nginx
 
+#Certbot for generating HTTPS certificate and installing
+snap install core
+snap refresh core
+apt-get remove certbot
+snap install --classic certbot
+ln -s /snap/bin/certbot /usr/bin/certbot
+
 pip3 install virtualenv
 
 echo "All packages installed"
 
 virtualenv BPForwarderEnv
 
-#sudo -u ubuntu source BPForwarderEnv/bin/activate
 source BPForwarderEnv/bin/activate
 
 echo "Virtual Environment activated"
@@ -20,28 +26,27 @@ echo "Virtual Environment activated"
 
 /home/ubuntu/BPForwarder/BPForwarderEnv/bin/pip install gunicorn flask
 
-cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.10/app.py .
+cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.11/app.py .
 
-cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.10/wsgi.py .
+cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.11/wsgi.py .
 
-cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.10/BPFconf.JSON .
+cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.11/BPFconf.JSON .
 
-#sudo -u ubuntu deactivate
 deactivate
 echo "Virtual Environment deactivated"
 
 #Copy .service file to /etc/systemd/system/
-cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.10/BPForwarder.service /etc/systemd/system/
+cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.11/BPForwarder.service /etc/systemd/system/
 
 systemctl start BPForwarder
 
 systemctl enable BPForwarder
 
-systemctl status BPForwarder
+#systemctl status BPForwarder
 
 echo "Flask and Gunicorn configured"
 
-cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.9/BPForwarderNGINXserverblockconfigHTTP /etc/nginx/sites-available/
+cp /home/ubuntu/BPForwarder/BPForwarderPlatform-0.11/BPForwarderNGINXserverblockconfigHTTP /etc/nginx/sites-available/
 
 ln -s /etc/nginx/sites-available/BPForwarderNGINXserverblockconfigHTTP /etc/nginx/sites-enabled
 
@@ -49,7 +54,12 @@ rm /etc/nginx/sites-enabled/default
 
 nginx -t
 
+gpasswd -a www-data ubuntu
+
 systemctl restart nginx
+#nginx -s reload
+
+certbot --nginx
 
 ufw allow 'Nginx Full'
 
